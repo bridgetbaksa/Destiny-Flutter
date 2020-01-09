5436ae974840173b66cbccff74232cba7c143133
Bridget Baksa <bbaksa@deloitte.com>
2020-01-09 15:15:21 -0500

    First commit

---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+class Story {
+  String storyTitle = '';
+  String choice1 = '';
+  String choice2 = '';
+
+  Story(String t, String c1, String c2){
+    storyTitle = t;
+    choice1 = c1;
+    choice2 = c2;
+  }
+
+}
